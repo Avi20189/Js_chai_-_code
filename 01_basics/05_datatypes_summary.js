1dc24a12199b85e5cfadb6c02c673973b9443cfd
Avinash <39276387+Avi20189@users.noreply.github.com>
2024-04-30 12:02:16 +0530

    datatypes completed

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,33 @@
+// primitive
+
+// 7 types : string, number , boolean, Null, undefined, symbol, BigInt
+
+//Refrence  (Non Primitive)
+
+//Array , Objects, Functions
+
+const outSideTemp = null
+let userEmail
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 546328749453196789536907n;
+
+console.log(typeof bigNumber);
+
+
+
+//Array , Objects, Functions
+
+const boys = ["aman", "ayush", "avinash"];
+
+let myObj = {
+    name: "Avinash",
+    sex: "male"
+}
+
+
+
