9c549acf94223b92255f8775492945cd8d5408e5
Avinash <39276387+Avi20189@users.noreply.github.com>
2024-05-04 19:57:31 +0530

    Opearators khatam

---
+++ 01_basics/15_js_execution.js
@@ -0,0 +1,11 @@
+//If we run any function or any variable . It firstly goes to Global execution context which refers it to (this)
+//value of this is window object
+//After global execution context there is function execution context 
+//sometimes there is another context called Eval execution context which is used in mongoose.
+
+
+//javascript code runs in two phases 
+//Memory allocation phase and execution phase
+// 1.Global execution phase  ----> Memory allocation phase ----> Execution phase
+
+
---
+++ 01_basics/16_control_flow.js
@@ -0,0 +1,32 @@
+// const isLoggedIn = true;
+
+// if(!isLoggedIn){
+//     console.log("user is logged in")
+// }
+
+//falsy values :--- false, 0, -0, BigInt, 0n, "", null, ubdefined, NaN
+
+//truthy values :--- "0", 'false', " ", [], {}, function(){}
+
+
+//Nullish coalescing operator(??): null undefined
+
+let val1;
+
+// val1 =  5 ?? 10
+// val1 = null ?? 12 //This operator has been created for this specific task that if there will be null
+                  //null and undefined at first null and undefined and after ?? ther is value value will beassigned
+
+//val1 = undefined ?? 23                 
+
+console.log(val1);
+
+
+//Terniry operator
+
+// Condition ? true : false 
+
+const ferrari = 8000000
+
+ferrari >= 900000000  ?console.log("you can not buy")  : console.log("buy it");
+
