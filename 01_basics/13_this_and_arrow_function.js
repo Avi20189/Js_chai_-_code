c65c1a3fdc1ee99ef887c9c7031b23173b13baa7
Avinash <39276387+Avi20189@users.noreply.github.com>
2024-05-04 10:10:01 +0530

    js execution done

--- 01_basics/13_this_and_arrow_function.js
+++ 01_basics/13_this_and_arrow_function.js
@@ -6,14 +6,40 @@
 //     }
 // }
 
-const chai = function(){
-    let username = "avinash"
-    console.log(this.username);
-}
+// const chai = function(){
+//     let username = "avinash"
+//     console.log(this.username);
+// }
 
-chai();
+// chai();
 
-const chai1 = () => {
-   let username = "avisaas"
-   console.log(this.username);
-}
+// const chai1 = () => {
+//    let username = "avisaas"
+//    console.log(this.username);
+// }
+
+//Write an arrow function to add two numbers and print their value
+
+// const addTwo = (num1, num2) => {
+//    return num1+num2;
+// }
+
+// console.log(addTwo(3,4));
+
+//Now we have to implement a new type of output. It is called implicit return. In this 
+
+const addTwo = (num1,num2) => ({username: "aviansh"})
+
+
+console.log(addTwo(223,231));
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01_basics/14_IIFE.js
@@ -0,0 +1,34 @@
+//IIFE - It stands for immediately invoked function expression. 
+//We can achieve it by doing many ways but why do we need it. It is basically used in database code files.
+//So when our application starts it immediaetly executes that file.
+//We have to just wrap the function into () to execute that function without using node.js
+
+// Normal function 
+
+// function khsuhi() {
+//     console.log("Tashu ko peeto");
+// }
+
+//THis is normal function
+
+//To make this function IIFE we will wrap into ()
+
+
+//Named IIFE
+
+// (function khsuhi() {
+//     console.log("BadriNath ko peeto");
+// })()
+
+//same function in arrow function 
+
+
+//Un Named IIFE
+// ( (name) => {
+//     console.log(`dhyey pitai khayega ${name}`)
+// })("tashu se")
+
+
+
+
+
