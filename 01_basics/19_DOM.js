993b96f1fa4148305fd985d3db2062e1514b1a29
Avinash <39276387+Avi20189@users.noreply.github.com>
2024-05-14 20:42:28 +0530

    dom selecgor nodes

--- 01_basics/19_DOM.js
+++ 01_basics/19_DOM.js
@@ -15,4 +15,17 @@
 
 /* ***********************         DOM Selectors             ******************************/
 
+//It stands for DOM. it is used to manipulate functions in window object. it can be heavily used to 
+// implement functions, apply logics, and do server side rendering and client side rendering
+//it is a light weight language to do manipulations and complete all the funcitons and do it
+//It has many commands to select modify and implement designs wich effect our frontend and 
+//backends, like we use multiple commands for doing the things in the window objects.
+//All the selectors command need to be written in case senseitive way because it will
+//there are three ways to select and modify things:- 1.> Query selector  2.> Dom commands
+// 3.>Node
 
+//Nodes can have more than one lchild.
+//There are many ways to cahnge the tings:-- id, tagname, classname, byname, queryselector, queryselectorall
+//there are parent elements child elements 
+//There are many ways to manipulate DOM
+//Create a new elements
