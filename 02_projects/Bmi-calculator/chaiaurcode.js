5f207cbf270c0397e39c5b2e00b07d2ec0696265
Avinash <39276387+Avi20189@users.noreply.github.com>
2024-06-03 23:54:03 +0530

    Bmi calculator

---
+++ 02_projects/Bmi-calculator/chaiaurcode.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+   const height =  parseInt(document.querySelector('#height').value)
+   const weight =  parseInt(document.querySelector('#weight').value)
+   const results = document.querySelector('#results') 
+
+})
--- 02_projects/color-changer/chaiaurcode.js
+++ 02_projects/color-changer/chaiaurcode.js
@@ -7,6 +7,18 @@ buttons.forEach(function(button){
     //add an eventListner
     button.addEventListener('click', function(e){
          console.log(e);
-         console.log(e.target)
+         console.log(e.target);
+         if(e.target.id === 'grey'){
+            body.style.backgroundColor = e.target.id;
+         }
+         if(e.target.id === 'white'){
+            body.style.backgroundColor = e.target.id;
+         }
+         if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id;
+         }
+         if(e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id;
+         }
     })
 });
