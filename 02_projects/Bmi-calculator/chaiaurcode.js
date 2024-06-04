d3230d4c0af3e3e619d686fd7a6b8bec20abc752
Avinash <39276387+Avi20189@users.noreply.github.com>
2024-06-04 23:50:46 +0530

    consistency

--- 02_projects/Bmi-calculator/chaiaurcode.js
+++ 02_projects/Bmi-calculator/chaiaurcode.js
@@ -6,8 +6,6 @@ form.addEventListener('submit', function(e){
    const height =  parseInt(document.querySelector('#height').value)
    const weight =  parseInt(document.querySelector('#weight').value)
    const results = document.querySelector('#results') 
-   if(){
-    
-   }
+   
 
 })
