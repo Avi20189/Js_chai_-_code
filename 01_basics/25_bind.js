7c52127e38ea2c5cb48a14ffa4be8ff41b9b68ab
Avinash <39276387+Avi20189@users.noreply.github.com>
2024-06-16 23:50:54 +0530

    call method added

--- 01_basics/25_bind.js
+++ 01_basics/25_bind.js
@@ -1,5 +1,16 @@
 function setUsername(username){
     //complex DB calls 
-    this.username = username
+    this.username = username;
+    console.log('called')
+
 }
 
+function createUser(username, email, password){
+    setUsername.call(this,username)
+    this.email = email;
+    this.password = password;
+}
+
+const Avi = new createUser("Avinash", "Avinash@google.com", "googly")
+console.log(Avi)
+//What is bind function in javascript
