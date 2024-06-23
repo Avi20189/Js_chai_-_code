962c0a8b5e06e56aaafcfbd54fef5428d20dca64
Avinash <39276387+Avi20189@users.noreply.github.com>
2024-06-23 22:02:01 +0530

    classes anfd bind

--- 01_basics/26_classes.js
+++ 01_basics/26_classes.js
@@ -1,2 +1,43 @@
 //In javascript everything is object except function is 
-//
+//ES6
+
+class user {
+    constructor(username, email, password) {
+        this.username = username;
+        this.email = email;
+        this.password = password
+    }
+
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+
+    changeUsername(){
+        return `${this.username.toUpperCase()}`
+    }
+}
+
+const aston = new User("aston", "aston@gmail.com", "123")
+
+console.log(aston.encryptPassword());
+console.log(aston.changeUsername());
+
+
+
+//behind the scene
+
+function User(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+
+
+const tea = new User("lambo", "lanbo#gmail.com", "234")
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername()); 
